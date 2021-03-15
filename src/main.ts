f9cc8d515dd26598a9c6d63c1d0206e296c8bada
Synthhex <hexadecimal.overflow@gmail.com>
2021-03-15 14:49:45 +0200

    Added response messages and codes.

--- src/main.ts
+++ src/main.ts
@@ -19,30 +19,35 @@ app.post("/login", async function(req, res) {
     switch (auth.code) {
         case 500:
             res.status(500).send({
-                response: "INTERNAL_ERROR"
+                code: -1,
+                response: "Internal server error. Your query could not be completed."
             });
             return;
         case 200:
             const session = server.createSession(auth.user);
             res.status(200)
             .send({
-                response: "LOGIN_SUCCESSFUL",
+                code: 3,
+                response: "Login successful.",
                 sesh_token: session.token
             });
             return;
         case 400:
             res.status(200).send({
-                response: "BAD_REQUEST"
+                code: 0,
+                response: "Bad request."
             });
             return;
         case 404:
             res.status(200).send({
-                response: "USER_NOT_FOUND"
+                code: 1,
+                response: "Unknown user."
             });
             return;
         case 401:
             res.status(200).send({
-                response: "INCORRECT_PASSWORD"
+                code: 2,
+                response: "Incorrect password."
             });
             return;
     }
